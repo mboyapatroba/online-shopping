5ee8028007a5e4ae79872fb66a80dbf8681a1a68
mboyapatroba <mboyapatroba1@gmail.com>
2022-08-01 23:38:32 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="img/lifestyleStore.png" />
+        <title>Flamez Store</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <!-- jquery library -->
+        <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
+        <!-- Latest compiled and minified javascript -->
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <!-- External CSS -->
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <div>
+           <?php
+            require 'header.php';
+           ?>
+           <div id="bannerImage">
+               <div class="container">
+                   <center>
+                   <div id="bannerContent">
+                       <h1>We sell .</h1>
+                       <p>Flat 40% OFF on all premium brands.</p>
+                       <a href="products.php" class="btn btn-danger">Shop Now</a>
+                   </div>
+                   </center>
+               </div>
+           </div>
+           <div class="container">
+               <div class="row">
+                   <div class="col-xs-4">
+                       <div  class="thumbnail">
+                           <a href="products.php">
+                                <img src="img/tv.jpg" alt="Television">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                        <p id="autoResize">Television</p>
+                                        <p>Choose among the best available in the world.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/ps.jpg" alt="ps">
+                           </a>
+                           <center>
+                                <div class="caption">
+                                    <p id="autoResize">Playstations</p>
+                                    <p>Original playstations from the best brands.</p>
+                                </div>
+                           </center>
+                       </div>
+                   </div>
+                   <div class="col-xs-4">
+                       <div class="thumbnail">
+                           <a href="products.php">
+                               <img src="img/phone.jpg" alt="phone">
+                           </a>
+                           <center>
+                               <div class="caption">
+                                   <p id="autoResize">iphones</p>
+                                   <p>New brands of iphones available.</p>
+                               </div>
+                           </center>
+                       </div>
+                   </div>
+               </div>
+           </div>
+            <br><br> <br><br><br><br>
+           <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy <a href="https://flamez">Flamez</a> Store. All Rights Reserved.</p>
+                   <p>This website is developed by Flamez department</p>
+               </center>
+               </div>
+           </footer>
+        </div>
+    </body>
+</html>
